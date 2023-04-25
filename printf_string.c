e25d2e4029d3b3bf2b274a57a02e455f8d0a147f
elvisndungu <senkutoshi@gmail.com>
2023-04-25 08:49:27 +0200

    holy

--- printf_string.c
+++ printf_string.c
@@ -5,23 +5,25 @@
  * Return: the length of the string.
  */
 int printf_string(va_list args)
+{
+    char *s;
+    int i, len;
 
-{
-	const char *str = va_arg(args, const char *);
-	int count;
+    s = va_arg(args, char *);
 
-	if (str == NULL)
-{
-	return fputs("(null)", stdout);
-}
-	count = 0;
-    
-	while (*str != '\0')
-{
-	putchar(*str);
-	str++;
-	count++;
-	}
-	
-return count;
+    if (s == NULL)
+    {
+        s = "(null)";
+        len = _strlen(s);
+        for (i = 0; i < len; i++)
+            _putchar(s[i]);
+        return (len);
+    }
+    else
+    {
+        len = _strlen(s);
+        for (i = 0; i < len; i++)
+            _putchar(s[i]);
+        return (len);
+    }
 }
