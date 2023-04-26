9a9214be332c7a785008ad92c2caea158ed018e7
elvisndungu <senkutoshi@gmail.com>
2023-04-26 05:52:12 +0200

    import errors patch

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
 
 typedef struct
 	{
