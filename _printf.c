13b576896a731c8a93dc0d56198119468d64aa5b
elvisndungu <senkutoshi@gmail.com>
2023-04-26 04:40:58 +0200

    patch

--- _printf.c
+++ _printf.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
 #include <string.h>
-
 /**
  * _printf - Mother Function Calls other functions.
  * @format: identifier to look for.
@@ -9,39 +7,40 @@
  */
 int _printf(const char * const format, ...)
 {
-	static const match_maker match[] = {
-			{"%s", printf_string}, {"%c", printf_char}, {"%%", printf_percent},
-			{"%d", printf_integer}, {"%i", printf_integer}
-	};
-	int len = 0;
-	va_list args;
+    static const match_maker match[] = {
+            {"%s", printf_string}, {"%c", printf_char}, {"%%", printf_percent},
+            {"%d", printf_integer}, {"%i", printf_integer}
+    };
+    int len = 0;
+    va_list args;
+    int i, j;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+    va_start(args, format);
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
 
-	for (int i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			int found = 0;
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            int found = 0;
 
-			for (int j = 0; j < sizeof(match) / sizeof(match[0]); j++)
-			{
-				if (strncmp(match[j].id, format + i, strlen(match[j].id)) == 0)
-				{
-					len += match[j].f(args);
-					i += strlen(match[j].id) - 1;
-					found = 1;
-					break;
-				}
-			}
-			if (!found)
-				len += _putchar(format[i]);
-		}
-		else
-			len += _putchar(format[i]);
-	}
-	va_end(args);
-	return (len);
+            for (j = 0; j < sizeof(match) / sizeof(match[0]); j++)
+            {
+                if (strncmp(match[j].id, format + i, strlen(match[j].id)) == 0)
+                {
+                    len += match[j].f(args);
+                    i += strlen(match[j].id) - 1;
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found)
+                len += _putchar(format[i]);
+        }
+        else
+            len += _putchar(format[i]);
+    }
+    va_end(args);
+    return (len);
 }
