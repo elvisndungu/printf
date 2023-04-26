3d4fb7c1f41c05771cecfb10fce6e3071918ccec
elvisndungu <senkutoshi@gmail.com>
2023-04-26 05:22:05 +0200

    optimisation

--- _printf.c
+++ _printf.c
@@ -50,6 +50,7 @@ int _printf(const char *format, ...)
 		{
 			int match_idx = -1;
 			match_idx = m_identfy(format, match, m_len, i);
+
 			if (match_idx >= 0)
 			{
 				len += match[match_idx].f(args);
