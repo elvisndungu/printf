d09e18dece20a374a00cab30cd487cae283f9bea
elvisndungu <senkutoshi@gmail.com>
2023-04-27 15:58:05 +0200

    TABS TABS TABS I'm FINALLY DONE

--- ROT13.c
+++ ROT13.c
@@ -7,31 +7,31 @@
  */
 int ROT13(va_list args)
 {
-    int i, j, counter = 0;
-    int k = 0;
-    char *s = va_arg(args, char*);
-    char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
-    if (s == NULL)
-        s = "(null)";
-    for (i = 0; s[i]; i++)
-    {
-        k = 0;
-        for (j = 0; alpha[j] && !k; j++)
-        {
-            if (s[i] == alpha[j])
-            {
-                _putchar(beta[j]);
-                counter++;
-                k = 1;
-            }
-        }
-        if (!k)
-        {
-            _putchar(s[i]);
-            counter++;
-        }
-    }
-    return (counter);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
 }
