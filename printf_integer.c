519b2da42506be4b973954d0b15f05a29c8db6ac
elvisndungu <senkutoshi@gmail.com>
2023-04-27 15:24:19 +0200

    ok

--- printf_integer.c
+++ printf_integer.c
@@ -7,47 +7,84 @@
  */
 int printf_integer(va_list args)
 {
-	int num = va_arg(args, int);
-	int length = 0;
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
 
-	if (num == 0)
-	{
-		_putchar('0');
-		length = 1;
-	}
+	n = n / 10;
+	num = n;
 
-	else if (num < 0)
+	if (last < 0)
 	{
 		_putchar('-');
-		length = printf_positive_integer(-num);
-		length += 1;
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
 	}
-	else
+	if (num > 0)
 	{
-		length = printf_positive_integer(num);
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
 	}
+	_putchar(last + '0');
 
-	return (length);
+	return (i);
 }
 
 /**
- * printf_positive_integer - prints the absolute value of an integer to stdout
- * @num: the integer to print
- *
- * Return: the number of characters printed
+ * printf_decimal - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed
  */
-int printf_positive_integer(int num)
+int printf_decimal(va_list args)
 {
-	int length = 0;
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int  i = 1;
+	int exp = 1;
 
-	if (num / 10)
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
 	{
-		length = printf_positive_integer(num / 10);
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
 	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
 
-	/* betty patch */
-	_putchar((num % 10) + '0');
-	length += 1;
-
-	return (length);
+	return (i);
 }
